90252ce2e6cd427b5712206a89c4898e6f6ca425
0x24d <daniel.longfield@tuta.io>
2021-04-10 15:25:43 +0100

    Add barebones TCP server

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,20 @@
+#include <boost/asio.hpp>
+#include <string>
+
+using boost::asio::ip::tcp;
+
 int main() {
+    
+    boost::asio::io_context ctx;
+    tcp::acceptor acceptor(ctx, tcp::endpoint(tcp::v4(), 6379));
+    
+    while (true) {
+        tcp::socket socket(ctx);
+        acceptor.accept(socket);
+
+        boost::system::error_code ec;
+        boost::asio::write(socket, boost::asio::buffer("New client accepted\n"), ec);
+    }
+
     return 0;
 }
