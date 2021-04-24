11cf3807701c7fc0f58844a56a39697269fbf817
0x24d <daniel.longfield@tuta.io>
2021-04-24 20:38:38 +0100

    Add tests for Array of Bulk Strings, plus output number of tests passed/failed

--- test/testhelper.h
+++ test/testhelper.h
@@ -1,12 +1,20 @@
 #include <iostream>
-
+unsigned int testsRan = 0;
+unsigned int testsFailed = 0;
 class TestHelper {
 public:
     static void assert(const std::string& label, const auto& response, const auto& expectedResponse) {
-        if (response == expectedResponse) {
-            std::cout << label << ": " << "Pass\n";
+        if (response != expectedResponse) {
+            std::cerr << "Test case '" << label << "' failed\n";
+            ++testsFailed;
+        }
+        ++testsRan;
+    }
+    static void result() {
+        if (testsFailed == 0) {
+            std::cout << "All test cases passed\n";
         } else {
-            std::cerr << label << ": " << "Fail\n";
+            std::cerr << '\n' << std::to_string(testsFailed) << " test cases failed\n";
         }
     }
 };
