1598c7e9cf9723737134965fe0b21362f368bd94
0x24d <daniel.longfield@tuta.io>
2021-05-01 20:06:00 +0100

    Correct function names

--- test/unit/respparser.cpp
+++ test/unit/respparser.cpp
@@ -2,13 +2,6 @@
 #include "../utils/testhelper.h"
 
 void testArray() {
-    TestHelper::equals("Bulk String", RESPParser::parseRequest<DataTypes::BulkString>(RecvBuffer{"$5\r\nHello\r\n"}, -1).value(), std::vector{std::string{"Hello"}});
-    TestHelper::equals("Bulk String with Space", RESPParser::parseRequest<DataTypes::BulkString>(RecvBuffer{"$11\r\nHello World\r\n"}, -1).value(), std::vector{std::string{"Hello World"}});
-    TestHelper::equals("Bulk String with Newline (Quoted)", RESPParser::parseRequest<DataTypes::BulkString>(RecvBuffer{"$12\r\nHello\r\nWorld\r\n"}, -1).value(), std::vector{std::string{"Hello\r\nWorld"}});
-    TestHelper::equals("Bulk String with Newline (Unquoted)", RESPParser::parseRequest<DataTypes::BulkString>(RecvBuffer{"$14\r\nHello\\r\\nWorld\r\n"}, -1).value(), std::vector{std::string{"Hello\\r\\nWorld"}});
-}
-
-void testBulkString() {
     TestHelper::equals("Array of 1 Bulk String", RESPParser::parseRequest<DataTypes::Array>(RecvBuffer{"*1\r\n$5\r\nHello\r\n"}, -1).value(), std::vector{std::string{"Hello"}});
     TestHelper::equals("Array of 2 Bulk Strings", RESPParser::parseRequest<DataTypes::Array>(RecvBuffer{"*2\r\n$5\r\nHello\r\n$5\r\nWorld\r\n"}, -1).value(), std::vector{std::string{"Hello"}, std::string{"World"}});
     TestHelper::equals("Array of 1 Bulk String with Space", RESPParser::parseRequest<DataTypes::Array>(RecvBuffer{"*1\r\n$11\r\nHello World\r\n"}, -1).value(), std::vector{std::string{"Hello World"}});
@@ -16,6 +9,13 @@ void testBulkString() {
     
     TestHelper::equals("Array of 1 Bulk String with Newline", RESPParser::parseRequest<DataTypes::Array>(RecvBuffer{"*1\r\n$12\r\nHello\r\nWorld\r\n"}, -1).value(), std::vector{std::string{"Hello\r\nWorld"}});
     TestHelper::equals("Array of 2 Bulk Strings with Newline", RESPParser::parseRequest<DataTypes::Array>(RecvBuffer{"*2\r\n$12\r\nHello\r\nWorld\r\n$12\r\nWorld\r\nHello\r\n"}, -1).value(), std::vector{std::string{"Hello\r\nWorld"}, std::string{"World\r\nHello"}});
+}
+
+void testBulkString() {
+    TestHelper::equals("Bulk String", RESPParser::parseRequest<DataTypes::BulkString>(RecvBuffer{"$5\r\nHello\r\n"}, -1).value(), std::vector{std::string{"Hello"}});
+    TestHelper::equals("Bulk String with Space", RESPParser::parseRequest<DataTypes::BulkString>(RecvBuffer{"$11\r\nHello World\r\n"}, -1).value(), std::vector{std::string{"Hello World"}});
+    TestHelper::equals("Bulk String with Newline (Quoted)", RESPParser::parseRequest<DataTypes::BulkString>(RecvBuffer{"$12\r\nHello\r\nWorld\r\n"}, -1).value(), std::vector{std::string{"Hello\r\nWorld"}});
+    TestHelper::equals("Bulk String with Newline (Unquoted)", RESPParser::parseRequest<DataTypes::BulkString>(RecvBuffer{"$14\r\nHello\\r\\nWorld\r\n"}, -1).value(), std::vector{std::string{"Hello\\r\\nWorld"}});
 }
 
 int main() {
